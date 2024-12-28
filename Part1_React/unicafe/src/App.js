2393976363cf7218afc8009e741222b4ebef31c2
magnofilipe <filipemagno.contato@gmail.com>
2024-12-28 20:01:32 -0300

    feat: unicafe 1.10

--- Part1_React/unicafe/src/App.js
+++ Part1_React/unicafe/src/App.js
@@ -4,8 +4,12 @@ const Title = ({text}) => (
   <h1>{text}</h1>
 )
 
-const Feedback = ({handleClick, text}) => (
+const Button = ({handleClick, text}) => (
   <button onClick={handleClick}>{text}</button>
+)
+
+const StatisticLine = ({text, value}) => (
+  <p>{text} {value}</p>
 )
 
 const Statistics = (props) => {
@@ -14,47 +18,47 @@ const Statistics = (props) => {
   }
   return (
   <>
-    <p>good {props.good}</p>
-    <p>neutral {props.neutral}</p>
-    <p>bad {props.bad}</p>
-    <p>all {props.all}</p>
-    <p>average {props.average}</p>
-    <p>positive {props.positive} %</p>
+    <StatisticLine text="good" value={props.good}></StatisticLine>
+    <StatisticLine text="neutral" value={props.neutral}></StatisticLine>
+    <StatisticLine text="bad" value={props.bad}></StatisticLine>
+    <StatisticLine text="all" value={props.all}></StatisticLine>
+    <StatisticLine text="average" value={props.average}></StatisticLine>
+    <StatisticLine text="positive" value={props.positive}></StatisticLine>
   </>
   )
 }
 
 const App = () => {
-  const [feedback, setFeedback] = useState({
+  const [button, setButton] = useState({
     good: 0, neutral: 0, bad: 0, scoreboard: 0
   })
 
-  const handleGoodFeedback = () => 
-    setFeedback({...feedback, good: feedback.good + 1, scoreboard: feedback.scoreboard + 1})
+  const handleGoodButton = () => 
+    setButton({...button, good: button.good + 1, scoreboard: button.scoreboard + 1})
 
-  const handleNeutralFeedback = () => 
-    setFeedback({...feedback, neutral: feedback.neutral + 1})
+  const handleNeutralButton = () => 
+    setButton({...button, neutral: button.neutral + 1})
 
-  const handleBadFeedback = () => 
-    setFeedback({...feedback, bad: feedback.bad + 1, scoreboard: feedback.scoreboard - 1})
+  const handleBadButton = () => 
+    setButton({...button, bad: button.bad + 1, scoreboard: button.scoreboard - 1})
 
   // Causes NaN
-  const total = feedback.bad + feedback.neutral + feedback.good
-  const average = feedback.scoreboard / total
-  const positive = (feedback.good / total * 100)
+  const total = button.bad + button.neutral + button.good
+  const average = button.scoreboard / total
+  const positive = (button.good / total * 100) + "%"
 
   return (
     <div>
-      <Title text="Give Feedback"></Title>
-      <Feedback handleClick={handleGoodFeedback} text={"good"}></Feedback>
-      <Feedback handleClick={handleNeutralFeedback} text={"neutral"}></Feedback>
-      <Feedback handleClick={handleBadFeedback} text={"bad"}></Feedback>
+      <Title text="Give Button"></Title>
+      <Button handleClick={handleGoodButton} text={"good"}></Button>
+      <Button handleClick={handleNeutralButton} text={"neutral"}></Button>
+      <Button handleClick={handleBadButton} text={"bad"}></Button>
 
       <Title text="Statistics"></Title>
       <Statistics 
-        good={feedback.good}
-        neutral={feedback.neutral}
-        bad={feedback.bad}
+        good={button.good}
+        neutral={button.neutral}
+        bad={button.bad}
         all={total}
         average={average}
         positive={positive}
