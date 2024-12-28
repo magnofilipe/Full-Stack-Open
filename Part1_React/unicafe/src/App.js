8fd6d866a29098a20a1b911145f8123be312a031
magnofilipe <filipemagno.contato@gmail.com>
2024-12-27 21:25:02 -0300

    feat: 1.7 unicafe solved

--- Part1_React/unicafe/src/App.js
+++ Part1_React/unicafe/src/App.js
@@ -10,17 +10,22 @@ const Feedback = ({handleClick, text}) => (
 
 const App = () => {
   const [feedback, setFeedback] = useState({
-    good: 0, neutral: 0, bad: 0
+    good: 0, neutral: 0, bad: 0, scoreboard: 0
   })
 
   const handleGoodFeedback = () => 
-    setFeedback({...feedback, good: feedback.good + 1})
+    setFeedback({...feedback, good: feedback.good + 1, scoreboard: feedback.scoreboard + 1})
 
   const handleNeutralFeedback = () => 
     setFeedback({...feedback, neutral: feedback.neutral + 1})
 
   const handleBadFeedback = () => 
-    setFeedback({...feedback, bad: feedback.bad + 1})
+    setFeedback({...feedback, bad: feedback.bad + 1, scoreboard: feedback.scoreboard - 1})
+
+  // Causes NaN
+  const total = feedback.bad + feedback.neutral + feedback.good
+  const average = feedback.scoreboard / total
+  const positive = (feedback.good / total * 100)
 
   return (
     <div>
@@ -33,6 +38,9 @@ const App = () => {
       <p>good {feedback.good}</p>
       <p>neutral {feedback.neutral}</p>
       <p>bad {feedback.bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
