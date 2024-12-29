599221cb77b3d1623161dd2eaaf2d5f4a121b300
magnofilipe <filipemagno.contato@gmail.com>
2024-12-28 22:32:08 -0300

    feat: anecdotes: 1.12

---
+++ Part1_React/anecdotes/src/App.js
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+function getRandomInteger(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+
+  return Math.floor(Math.random() * (max - min)) + min
+}
+
+const Button = ({handleClick, text}) => {
+  return <button onClick={handleClick}>{text}</button>
+}
+
+const App = () => {
+  const anecdotes = [
+    'Se fazer algo dói, faça isso com mais frequência.',
+    'Contratar mão de obra para um projeto de software que já está atrasado, faz com que se atrase mais ainda!',
+    'Os primeiros 90% do código correspondem aos primeiros 10% do tempo de desenvolvimento... Os outros 10% do código correspondem aos outros 90% do tempo de desenvolvimento.',
+    'Qualquer tolo escreve código que um computador consegue entender. Bons programadores escrevem código que humanos conseguem entender.',
+    'Otimização prematura é a raiz de todo o mal.',
+    'Antes de mais nada, depurar é duas vezes mais difícil do que escrever o código. Portanto, se você escrever o código da forma mais inteligente possível, você, por definição, não é inteligente o suficiente para depurá-lo.',
+    'Programar sem o uso extremamente intenso do console.log é o mesmo que um médico se recusar a usar raio-x ou testes sanguíneos ao diagnosticar pacientes.',
+    'A única maneira de ir rápido é ir bem.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+
+  const handleClick = () => {
+    const updateAnecdote = getRandomInteger(0, 7)
+    setSelected(updateAnecdote)
+  }
+
+  return (
+    <>
+      <h3>{anecdotes[selected]}</h3>
+      <Button handleClick={handleClick} text={"next anectode"}></Button>
+    </>
+  )
+}
+
+export default App
